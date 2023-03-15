a17d3263304b2d809fe75f672ffa9ad534ac773e
Karan Meghe <karanmeghe87@gmail.com>
2023-03-15 13:13:38 +0530

    Context provider is set

--- Food App/src/main.jsx
+++ Food App/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { AppProvider } from "./context";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const el = document.getElementById("root");
+const root = ReactDOM.createRoot(el);
+
+root.render(
+  <AppProvider>
     <App />
-  </React.StrictMode>,
-)
+  </AppProvider>
+);
